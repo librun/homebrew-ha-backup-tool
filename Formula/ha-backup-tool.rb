bb0ae7a25d0757f7974ea7bfcc5e8c6f7a1a62c8
Ilya Mazaev <limp2006@ya.ru>
2025-09-28 19:15:59 +0300

    add new-tap

---
+++ Formula/ha-backup-tool.rb
@@ -0,0 +1,25 @@
+class HaBackupTool < Formula
+  desc "Tool for work with Home Assistant Backup"
+  homepage "https://github.com/librun/ha-backup-tool"
+  url "https://github.com/librun/ha-backup-tool/archive/refs/tags/v1.4.4.tar.gz"
+  sha256 "a11ccb74921e354cfe4064bf4046f068d9c3508b3950c4a264c152acbf4d1dda"
+  license "MIT"
+  head "https://github.com/librun/ha-backup-tool.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %w[
+      -s -w
+    ]
+    tags = %w[extended withdeploy]
+    system "go", "build", *std_go_args(ldflags:, tags:)
+
+    generate_completions_from_executable(bin/"ha-backup-tool", "completion", shells: [:bash, :zsh, :fish])
+  end
+
+  test do
+    system bin/"ha-backup-tool", "--version"
+  end
+end
+
