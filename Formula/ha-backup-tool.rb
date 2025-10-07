62edb029888329009001093167857810c3550870
Ilya Mazaev <limp2006@ya.ru>
2025-10-08 00:53:18 +0300

    rebuild app

--- Formula/ha-backup-tool.rb
+++ Formula/ha-backup-tool.rb
@@ -6,11 +6,13 @@ class HaBackupTool < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/librun/ha-backup-tool/releases/download/v1.4.4"
-    sha256 cellar: :any_skip_relocation, arm64: "d711e9107bed674f23538f786d0fc333afa4f0478259c71a7cc7efb4eedd522d"
-    sha256 cellar: :any_skip_relocation, x86_64: "35aaf8215ce92588e0d1703c18cf41b0697bb0099edbe5adf9595136de67a29d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6f794cf54e09c90cb9f1890066d6aa01e95c537af6240329dcc15a8bf718a046"
-    sha256 cellar: :any_skip_relocation, arm64_linux: "50e46939caa4abb274a5497200d82b24fb4d0b33b7899d6f598c12c7df2f9038"
+    # rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7d362501e424487fc79e6e3d690e1f0c4642bff9bf9b450daf17ce1378c09be0"
+
+    #sha256 cellar: :any_skip_relocation, arm64: "d711e9107bed674f23538f786d0fc333afa4f0478259c71a7cc7efb4eedd522d"
+    #sha256 cellar: :any_skip_relocation, x86_64: "35aaf8215ce92588e0d1703c18cf41b0697bb0099edbe5adf9595136de67a29d"
+    #sha256 cellar: :any_skip_relocation, x86_64_linux: "6f794cf54e09c90cb9f1890066d6aa01e95c537af6240329dcc15a8bf718a046"
+    #sha256 cellar: :any_skip_relocation, arm64_linux: "50e46939caa4abb274a5497200d82b24fb4d0b33b7899d6f598c12c7df2f9038"
   end
 
   depends_on "go" => :build
